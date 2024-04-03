d38b5dc6fb8198932be0831a614c4396ebdbc51c
abhishekkumar24 <abhishek.kumar24@nagarro.com>
2024-04-03 18:06:37 +0530

    devops assignment3

--- src/main/java/com/nagarro/controller/DockerController.java
+++ src/main/java/com/nagarro/controller/DockerController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-public class DummyController {
+public class DockerController {
 	@GetMapping
 	public ResponseEntity<String> getMsg(){
 		return ResponseEntity.ok().body("This is an sample api for docker devops assignment");
+	}
+	@GetMapping("/api2")
+	public ResponseEntity<String> getMsg1(){
+		return ResponseEntity.ok().body("This is an api2 for docker devops assignment");
 	}
 }
